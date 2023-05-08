0ba0a69ae5abd2559350f0526db846bfd89ad6b6
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-08 09:41:47 -0700

    side nav

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './SideNav.css';
+import { MdDashboard as DashboardIcon } from 'react-icons/md';
 
 const SideNav = () => {
   return (
     <nav className="SideNav">
       <h1>BidSight</h1>
+
+      <div className='dashboard-container'>
+        <DashboardIcon className='nav-icon'/>
+        <h2 className='nav-label'>Dashboard</h2>
+      </div>
     </nav>
   );
 }
