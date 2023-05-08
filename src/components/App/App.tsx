add903c9a5d5f5e0ade036bbf164c3dc17704503
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-08 09:09:11 -0700

    header start

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,6 +8,12 @@ const App = () => {
         <p>
           Hi <strong>Tyler Corbett</strong>
         </p>
+        <div className='avatar-container'>
+          <img 
+            className='avatar'
+            src="https://res.cloudinary.com/dmtn5fbdu/image/upload/v1552429348/samples/people/smiling-man.jpg"
+          />
+        </div>
       </header>
     </div>
   );
