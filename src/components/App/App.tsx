6b5eadf04b282e2bf920530f07c7d5929615ee46
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-08 09:24:51 -0700

    left nav started

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import SideNav from '../SideNav/SideNav';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <header>
-        <p>
-          Hi <strong>Tyler Corbett</strong>
-        </p>
-        <div className='avatar-container'>
-          <img 
-            className='avatar'
-            src="https://res.cloudinary.com/dmtn5fbdu/image/upload/v1552429348/samples/people/smiling-man.jpg"
-          />
-        </div>
-      </header>
+      <SideNav />
+      <section>
+        <header>
+          <p>
+            Hi <strong>Tyler Corbett</strong>
+          </p>
+          <div className='avatar-container'>
+            <img 
+              className='avatar'
+              src="https://res.cloudinary.com/dmtn5fbdu/image/upload/v1552429348/samples/people/smiling-man.jpg"
+            />
+          </div>
+        </header>
+      </section>
     </div>
   );
 }
---
+++ src/components/SideNav/SideNav.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideNav.css';
+
+const SideNav = () => {
+  return (
+    <nav className="SideNav">
+      
+    </nav>
+  );
+}
+
+export default SideNav;
