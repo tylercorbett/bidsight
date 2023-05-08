efa76c469934af116efb7a81c48abff10c3e95c8
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-08 09:53:02 -0700

    basic component

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './SideNav.css';
 import { MdDashboard as DashboardIcon } from 'react-icons/md';
+import { BsChevronRight as ChevronRight } from 'react-icons/bs';
+import { AiFillPieChart as Chart } from 'react-icons/ai';
 
 const SideNav = () => {
   return (
     <nav className="SideNav">
       <h1>BidSight</h1>
-      <div className='dashboard-container'>
+      <div className='dashboard-container active'>
         <DashboardIcon className='nav-icon'/>
         <h2 className='nav-label'>Dashboard</h2>
+      </div>
+      <div className='nav-section'>
+        <p className='nav-section-label'>COMPONENTS</p>
+          <div className='dashboard-container spread-items'>
+            <span className='align-horizontal'>
+              <Chart className='nav-icon'/>
+              <h2 className='nav-label'>Charts</h2>
+            </span>
+            <ChevronRight />
+        </div>
       </div>
     </nav>
   );
