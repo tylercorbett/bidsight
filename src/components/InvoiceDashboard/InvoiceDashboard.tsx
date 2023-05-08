bed626e2e924aa917b7dd7a4a78338ebc292a699
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-08 11:16:32 -0700

    filter setup

--- src/components/InvoiceDashboard/InvoiceDashboard.tsx
+++ src/components/InvoiceDashboard/InvoiceDashboard.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './InvoiceDashboard.css';
 
 const InvoiceDashboard: React.FC = () => {
+  const [activeFilter, setActiveFilter] = useState('all');
   return (
     <section className="InvoiceDashboard">
       <h3 className='section-title'>Invoices</h3>
       <div className='filters'>
-        <button className='filter active-filter'>
+        <button className='filter active-filter' onClick={() => setActiveFilter('all')}>
           All
         </button>
-        <button className='filter'>
+        <button className='filter' onClick={() => setActiveFilter('due')}>
           Due
         </button>
-        <button className='filter'>
+        <button className='filter' onClick={() => setActiveFilter('paid')}>
           Paid
         </button>
-        <button className='filter'>
+        <button className='filter' onClick={() => setActiveFilter('unpaid')}>
           Unpaid
         </button>
-        <button className='filter'>
+        <button className='filter' onClick={() => setActiveFilter('late')}>
           Late
         </button>
       </div>
