493ab14a6e84bc1f4e06fe368473dee686768e2e
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-09 11:38:27 -0700

    cleanup

--- src/components/InvoiceTable/InvoiceTable.tsx
+++ src/components/InvoiceTable/InvoiceTable.tsx
@@ -8,11 +8,9 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { isLate } from '../../utils/isLate';
 
 function createData(
   name: string,
