197983b9ce44d07d8d8bc73d81b06365733f1553
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-08 17:26:52 -0700

    add draft filter

--- src/components/InvoiceFilters/InvoiceFilters.tsx
+++ src/components/InvoiceFilters/InvoiceFilters.tsx
@@ -5,22 +5,26 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import './InvoiceFilters.css';
 
 export default function InvoiceFilters() {
-  const [checked, setChecked] = useState([true, true, true]);
+  const [checked, setChecked] = useState([true, true, true, true]);
 
   const handleChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked([event.target.checked, event.target.checked, event.target.checked]);
+    setChecked([event.target.checked, event.target.checked, event.target.checked, event.target.checked]);
   };
 
   const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked([event.target.checked, checked[1], checked[2]]);
+    setChecked([event.target.checked, checked[1], checked[2], checked[3]]);
   };
 
   const handleChange3 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked([checked[0], event.target.checked, checked[2]]);
+    setChecked([checked[0], event.target.checked, checked[2], checked[3]]);
   };
 
   const handleChange4 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked([checked[0], checked[1], event.target.checked]);
+    setChecked([checked[0], checked[1], event.target.checked, checked[3]]);
+  };
+
+  const handleChange5 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked([checked[0], checked[1], checked[2], event.target.checked]);
   };
 
   const children = (
@@ -36,6 +40,10 @@ export default function InvoiceFilters() {
       <FormControlLabel
         label="Late"
         control={<Checkbox checked={checked[2]} onChange={handleChange4} />}
+      />
+      <FormControlLabel
+        label="Draft"
+        control={<Checkbox checked={checked[3]} onChange={handleChange5} />}
       />
     </Box>
   );
