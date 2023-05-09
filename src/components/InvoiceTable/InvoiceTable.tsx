e2ec770f3299b4f280136ec68cf025aa7fda72fb
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-09 12:53:12 -0700

    basic props in place

--- src/components/InvoiceTable/InvoiceTable.tsx
+++ src/components/InvoiceTable/InvoiceTable.tsx
@@ -11,42 +11,13 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { createData } from '../InvoiceDashboard/InvoiceDashboard';
 
-function createData(
-  name: string,
-  status: string,
-  category: string,
-  dueDate: string,
-  id: number,
-) {
-  return {
-    name,
-    status,
-    category,
-    dueDate,
-    id,
-    history: [
-      {
-        label: 'Drywall installation',
-        cost: '2800.00'
-      },
-      {
-        label: 'Carpentry work:',
-        cost: '1650.00'
-      },
-      {
-        label: 'Lumber wood 2x4',
-        cost: '1200.00'
-      },
-    ],
-  };
-}
-
-interface Props {
+interface RowProps {
   row: ReturnType<typeof createData>
 }
 
-const Row:React.FC<Props> = ({ row }) => {
+const Row:React.FC<RowProps> = ({ row }) => {
   const [open, setOpen] = useState(false);
 
   return (
@@ -101,20 +72,11 @@ const Row:React.FC<Props> = ({ row }) => {
   );
 }
 
-const rows = [
-  createData('Glass Facade', 'Paid', 'New Construction', '11/15/2022', 1),
-  createData('Water Damage Repair', 'Draft', 'Painting', '12/08/2022', 2),
-  createData('Window Replacement', 'Outstanding', 'Repairs', '01/25/2024', 3),
-  createData('Kitchen Renovation', 'Outstanding', 'Renovations', '04/25/2023', 4),
-  createData('HVAC Replacement', 'Outstanding', 'Repairs', '06/05/2022', 5),
-  createData('Paint Siding', 'Paid', 'Painting', '01/05/2023', 6),
-  createData('Loft Renovation', 'Draft', 'Renovations', '06/07/2023', 7),
-  createData('HVAC Replacement', 'Paid', 'Repairs', '03/05/2023', 8),
-  createData('HVAC Replacement', 'Paid', 'Repairs', '09/12/2023', 9),
-];
+interface InvoiceTableProps {
+  rows: any[]
+}
 
-
-const InvoiceTable = () => {
+const InvoiceTable: React.FC<InvoiceTableProps> = ({ rows }) => {
   return (
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
