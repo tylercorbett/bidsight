6398d6033bc2fbbbb4cdfcda4371c0f80cbb155a
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-10 08:17:45 -0700

    fix import

--- src/components/InvoiceFilters/InvoiceFilters.tsx
+++ src/components/InvoiceFilters/InvoiceFilters.tsx
@@ -1,4 +1,4 @@
-import Reactfrom 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
