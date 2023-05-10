e592e893c1198146ba054498a9174fb2997c54ac
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-10 12:42:30 -0700

    charges are formatting correct

--- src/utils/formatData.ts
+++ src/utils/formatData.ts
@@ -1,15 +1,22 @@
 import { Invoice } from "../types/invoice";
 import { capitalizeString } from "./capitalizeString";
 
-export const formatData = (data: Invoice[]): Invoice[] => {
+function reformatCharges(arr: { [key: string]: string }[]): { label: string; cost: string }[] {
+  return arr.map((obj) => {
+    const [label, cost] = Object.entries(obj)[0];
+    return { label, cost };
+  });
+}
+
+export const formatData = (data: any[]): Invoice[] => {
   const result: Invoice[] = [];
 
-  data.forEach((invoice: Invoice) => {
-    console.log('data in formatData', invoice);
+  data.forEach((invoice: any) => {
     const formattedInvoice = {...invoice};
     const capitalizedStatus = capitalizeString(invoice.status);
+    const reformattedCharges = reformatCharges(invoice.charges);
     formattedInvoice.status = capitalizedStatus;
-
+    formattedInvoice.charges = reformattedCharges;
     result.push(formattedInvoice)
   });
 
