cdcaa7fc60efc9ea25288369a7443ed7e3534230
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-10 16:14:57 -0700

    chart renders nicely

--- src/components/InvoicePieChart/InvoicePieChart.tsx
+++ src/components/InvoicePieChart/InvoicePieChart.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { PieChart, Pie, Cell } from 'recharts';
+import { PieChart, Pie, Cell, Tooltip } from 'recharts';
 
 const data = [
-  { name: 'Section 1', value: 30 },
-  { name: 'Section 2', value: 50 },
-  { name: 'Section 3', value: 20 },
+  { name: 'Section 1', value: 30, color: '#001f3f' },
+  { name: 'Section 2', value: 50, color: '#0074D9' },
+  { name: 'Section 3', value: 20, color: '#00A6FF' },
+  { name: 'Section 4', value: 40, color: '#48CAE4' },
+  { name: 'Section 5', value: 10, color: '#90E0EF' },
 ];
 
-const COLORS = ['#8884d8', '#82ca9d', '#ffc658'];
+const colors = ['#001f3f', '#0074D9', '#00A6FF', '#48CAE4', '#90E0EF'];
 
 const InvoicePieChart = () => {
   return (
@@ -18,13 +20,31 @@ const InvoicePieChart = () => {
         cy={200}
         outerRadius={80}
         fill="#8884d8"
-        label
+        label={({ cx, cy, midAngle, innerRadius, outerRadius, value, index }) => {
+          const RADIAN = Math.PI / 180;
+          const radius = 25 + innerRadius + (outerRadius - innerRadius);
+          const x = cx + radius * Math.cos(-midAngle * RADIAN);
+          const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+          return (
+            <text
+              x={x}
+              y={y}
+              fill={data[index].color}
+              textAnchor={x > cx ? 'start' : 'end'}
+              dominantBaseline="central"
+            >
+              {data[index].name}
+            </text>
+          );
+        }}
         dataKey="value"
       >
         {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
         ))}
       </Pie>
+      <Tooltip />
     </PieChart>
   );
 };
