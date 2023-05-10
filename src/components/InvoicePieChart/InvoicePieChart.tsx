59101579f0d4012ebc27f65a05472759c4a8e12b
Tyler Corbett <thetylercorbett@gmail.com>
2023-05-10 16:19:37 -0700

    chart works statically

--- src/components/InvoicePieChart/InvoicePieChart.tsx
+++ src/components/InvoicePieChart/InvoicePieChart.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { PieChart, Pie, Cell, Tooltip } from 'recharts';
 
 const data = [
@@ -9,7 +8,57 @@ const data = [
   { name: 'Section 5', value: 10, color: '#90E0EF' },
 ];
 
-const colors = ['#001f3f', '#0074D9', '#00A6FF', '#48CAE4', '#90E0EF'];
+// 50 colors currently supported - add more if needed
+const colors = [
+  '#002A5E',
+  '#094D96',
+  '#1C72CE',
+  '#389BF2',
+  '#58BDFC',
+  '#78D1FF',
+  '#99E5FF',
+  '#BBF0FF',
+  '#DFFCFF',
+  '#F4FFFF',
+  '#4C4CFF',
+  '#4C4CFF',
+  '#4C4CFF',
+  '#4C4CFF',
+  '#4C4CFF',
+  '#4C4CFF',
+  '#4C4CFF',
+  '#4C4CFF',
+  '#4C4CFF',
+  '#4C4CFF',
+  '#FF8C94',
+  '#FFA678',
+  '#FFC460',
+  '#FFDF54',
+  '#FFF248',
+  '#F9FF40',
+  '#D5FF40',
+  '#B0FF40',
+  '#8CFF40',
+  '#68FF40',
+  '#40FF5E',
+  '#40FF83',
+  '#40FFA9',
+  '#40FFCF',
+  '#40FFFF',
+  '#40E6FF',
+  '#40C1FF',
+  '#409DFF',
+  '#4078FF',
+  '#4054FF',
+  '#8340FF',
+  '#A940FF',
+  '#D040FF',
+  '#FF40FF',
+  '#FF40D0',
+  '#FF409D',
+  '#FF4068',
+  '#FF4034'
+];
 
 const InvoicePieChart = () => {
   return (
